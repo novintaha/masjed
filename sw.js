75c521e1240bcf01521cb0663c21e92f8522d037
novintaha <pastokala@gmail.com>
2026-08-09 10:11:46 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,34 @@
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDT0sQ_ZcT_Jl9_vvjgsgVojP_TCCLaEFM",
+  authDomain: "masjed-a6505.firebaseapp.com",
+  projectId: "masjed-a6505",
+  storageBucket: "masjed-a6505.firebasestorage.app",
+  messagingSenderId: "23821410712",
+  appId: "1:23821410712:web:1789bb8cc4659377544bed"
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(self.clients.claim());
+var fbMessagingSW = firebase.messaging();
+
+fbMessagingSW.onBackgroundMessage(function(payload) {
+  var n = payload.notification || {};
+  var d = payload.data || {};
+  var title = n.title || d.title || '🕌 مسجد آقا منیر';
+  var body = n.body || d.body || '';
+  self.registration.showNotification(title, {
+    body: body,
+    icon: 'https://i.ibb.co/S2b6YjQ/unnamed.jpg',
+    badge: 'https://i.ibb.co/S2b6YjQ/unnamed.jpg',
+    dir: 'rtl',
+    lang: 'fa',
+    vibrate: [200, 100, 200, 100, 200]
+  });
 });
 
+self.addEventListener('install', function(e) { self.skipWaiting(); });
+self.addEventListener('activate', function(e) { e.waitUntil(self.clients.claim()); });
 self.addEventListener('notificationclick', function(e) {
   e.notification.close();
   e.waitUntil(
