288b9a50e99b42dbbb9db61c83abf833213122d9
novintaha <pastokala@gmail.com>
2026-08-16 10:23:36 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'masjed-v99';
+var CACHE = 'masjed-v100';
 var SHELL = ['./', './index.html', './duas-data.js', './manifest.json'];
 var AZAN_CACHE = ['https://archive.org/download/adhan.notifications/Mishary_Rashid_al_Afasy_Fajr_Adhan.mp3'];
 
